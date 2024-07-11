d82bb807b5fae0964d4da7bb58aa5289b5c79324
K.Anuraga Sahoo <k.anuragsahoo@gmail.com>
2024-07-11 23:36:16 +0530

    Delete 01_variable.js

--- 01_variable.js
+++
@@ -1,8 +0,0 @@
-const accountid= 144553
-let accountEmail = "SKDJS@google.com"
-var accountPassword = "12345"
-accountcity = "ag"
-
-//accountid=2
-console.log(accountid)
-console.table([accountEmail,accountid,accountPassword,accountcity])
